0a7727a514a8b8bf28350aa6a8078ba71a938538
Brian29395048 <118739856+nuk3dev@users.noreply.github.com>
2024-04-30 06:28:05 +0200

    sql fix

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
+made to receive an exemption for subject. This is to show the back-end functions, not anything front-end.
+
+Routes
+
 login: /login
-dashboard: /dashboard
+manage albums/photos: /dashboard
+
+edit: dashboard/albums/editAlbum/1
+dashboard/albums/selectPhotobyalbum/1
+dashboard/albums/editPhoto/1
+
+delete: dashboard/albums/deletePhotoByAlbum/1
+
+
--- template.sql
+++ template.sql
@@ -4,7 +4,7 @@ CREATE DATABASE 'photo_gallery' IF NOT EXISTS;
 CREATE TABLE users (
         id INT AUTO_INCREMENT PRIMARY KEY,
         username VARCHAR(255) NOT NULL,
-        password VARCHAR(255) NOT NULL
+        password VARCHAR(255) NOT NULL,
         deleted TINYINT(2) NOT NULL DEFAULT '0'
 );
 
@@ -20,11 +20,11 @@ CREATE TABLE photos (
         id INT AUTO_INCREMENT PRIMARY KEY,
         title VARCHAR(255) NOT NULL,
         img_name VARCHAR(255) NOT NULL,
-        album_id INT NOT NULL
+        album_id INT NOT NULL,
         deleted TINYINT(2) NOT NULL DEFAULT '0'
 );
 
-INSERT INTO accounts (username, password)
+INSERT INTO users (username, password)
 VALUES ('test', '$2a$12$Ly8z2FVUHS0ZlvcL4909Seo1976VgSmyNrM6Z39/UaIR2Rnl2VTdC');
 
 INSERT INTO albums (album_name, cover_photo)
